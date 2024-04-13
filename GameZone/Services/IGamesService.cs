259eda3c7e71478f794f76804287564fae259465
Amira <amirahussein632@gmail.com>
2024-04-13 21:33:07 +0200

    first commit

---
+++ GameZone/Services/IGamesService.cs
@@ -0,0 +1,11 @@
+﻿namespace GameZone.Services;
+
+public interface IGamesService
+{
+    IEnumerable<Game> GetAll(); //games
+    Game? GetById(int id);
+    // to add new game in db
+    Task Create(CreateGameFormViewModel model);
+    Task<Game?> Update(EditGameFormViewModel model);
+    bool Delete(int id);
+}
